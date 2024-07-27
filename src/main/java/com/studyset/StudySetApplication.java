5bcc3218245268897b060e20e05f539b6ed36ee3
gagle1231 <kke1231@naver.com>
2024-07-27 17:58:41 +0900

    initial commit

---
+++ src/main/java/com/studyset/StudySetApplication.java
@@ -0,0 +1,13 @@
+package com.studyset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyset/StudySetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
