2bdb56fcfcb26a4a9413c03b8ed2c0364d99b8cb
gagle1231 <kke1231@naver.com>
2024-07-30 01:13:31 +0900

    feat: 스터디 그룹 검색 기능 #13

--- src/main/java/com/studyset/repository/UserJoinGroupRepository.java
+++ src/main/java/com/studyset/repository/UserJoinGroupRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserJoinGroupRepository extends JpaRepository<UserJoinGroup, Long> {
     //회원의 스터디 그룹 리스트 조회
     @Query("SELECT uj.group FROM UserJoinGroup uj WHERE uj.user.id = :userId")
-    Page<Group> findGroupsByUserId(Long userId, Pageable pageable);
+    Page<Group> findGroupsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT uj.group FROM UserJoinGroup uj WHERE uj.user.id = :userId AND uj.group.groupName LIKE CONCAT('%', :keyword, '%')")
+    Page<Group> findUserGroupBySearch(@Param("userId") Long userId, @Param("keyword") String keyword, Pageable pageable);
+
 }
