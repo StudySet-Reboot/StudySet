9159d876bbc9902c4e783e906eff678d064c0505
gagle1231 <kke1231@naver.com>
2024-08-03 01:32:00 +0900

    feat: group join 예외 처리 #18
    -ErrorResponse: api용 예외 응답 포맷
    -BaseException: 스터디셋 서비스 베이스 예외
    -GroupNotExist: 존재하지 않는 그룹
    -AlreadyJoin: 이미 가입한 그룹

--- src/main/java/com/studyset/api/exception/BaseException.java
+++ src/main/java/com/studyset/api/exception/BaseException.java
@@ -1,5 +1,23 @@
-package com.studyset.exception;
+package com.studyset.api.exception;
+
+import java.util.HashMap;
 
 public abstract class BaseException extends RuntimeException{
-    
+    private final HashMap<String, String> validation = new HashMap<>();
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int statusCode();
+    public void addValidation(String fieldName, String errMessage){
+        validation.put(fieldName, errMessage);
+    }
+
+    public HashMap<String, String> getValidation() {
+        return validation;
+    }
 }
