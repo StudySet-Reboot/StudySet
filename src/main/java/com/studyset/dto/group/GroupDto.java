83b2e1a12ed1a77f03ba0cfd1dfffa588bf65f81
hyooojing <81282601+hyooojing@users.noreply.github.com>
2024-08-04 17:51:11 +0900

    feat: groupMain 화면 연결
    그룹 정보 및 코드 조회
    groupMain 화면만

--- src/main/java/com/studyset/dto/group/GroupDto.java
+++ src/main/java/com/studyset/dto/group/GroupDto.java
@@ -7,13 +7,15 @@ import lombok.Data;
 
 @Data
 public class GroupDto {
+    private Long id;
     private String groupName;
     private GroupCategory category;
     private String description;
     private String code;
 
     @Builder
-    public GroupDto(String groupName, GroupCategory category, String description, String code) {
+    public GroupDto(Long id, String groupName, GroupCategory category, String description, String code) {
+        this.id = id;
         this.groupName = groupName;
         this.category = category;
         this.description = description;
