a2304af3c6c16e4542fbe663b993c6351afab4a1
hyooojing <81282601+hyooojing@users.noreply.github.com>
2024-08-08 01:30:25 +0900

    feat: 메모 전송 기능 #39
    메모 전송 완료
    메모 조회 시 스크롤 수정 필요

--- src/main/java/com/studyset/domain/Memo.java
+++ src/main/java/com/studyset/domain/Memo.java
@@ -2,9 +2,12 @@ package com.studyset.domain;
 
 import com.studyset.dto.memo.MemoDto;
 import jakarta.persistence.*;
-import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Setter
 public class Memo extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,17 +23,8 @@ public class Memo extends BaseEntity{
     @Column(nullable = false)
     private String contents;
 
-    @Builder
-    public Memo(Long id, Group group, User user, String contents) {
-        this.id = id;
-        this.group = group;
-        this.user = user;
-        this.contents = contents;
-    }
-
     public MemoDto toDto() {
         return MemoDto.builder()
-                .id(id)
                 .group(group)
                 .user(user)
                 .contents(contents)
