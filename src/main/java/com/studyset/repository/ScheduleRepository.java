151f314bd161bebbfb9281e5174a88ca95ef196d
kge <kke1231@naver.com>
2024-08-13 17:57:49 +0900

    feat: Schedule Event 생성 로직 추가 #42

--- src/main/java/com/studyset/repository/ScheduleRepository.java
+++ src/main/java/com/studyset/repository/ScheduleRepository.java
@@ -1,2 +1,14 @@
-package com.studyset.repository;public interface ScheduleRepository {
+package com.studyset.repository;
+
+import com.studyset.domain.Group;
+import com.studyset.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    public List<Schedule> findSchedulesByGroup_IdAndStartTimeBetween(long groupId, LocalDateTime startTime, LocalDateTime startTime2);
 }
