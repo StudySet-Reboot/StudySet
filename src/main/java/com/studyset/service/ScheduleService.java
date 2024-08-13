d54f6ab5bea5edf753c46785c6836dc050515578
kge <kke1231@naver.com>
2024-08-13 17:57:49 +0900

    feat: Schedule Event 생성 로직 추가 #42

--- src/main/java/com/studyset/service/ScheduleService.java
+++ src/main/java/com/studyset/service/ScheduleService.java
@@ -1,2 +1,47 @@
-package com.studyset.service;public class ScheduleService {
+package com.studyset.service;
+
+import com.studyset.api.exception.GroupNotExist;
+import com.studyset.domain.Group;
+import com.studyset.domain.Schedule;
+import com.studyset.dto.schedule.Event;
+import com.studyset.repository.GroupRepository;
+import com.studyset.repository.ScheduleRepository;
+import com.studyset.web.form.ScheduleCreateForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+    private final GroupRepository groupRepository;
+
+    @Transactional
+    public void addSchedule(ScheduleCreateForm scheduleCreateForm, long groupId) {
+        Group group = groupRepository.findGroupById(groupId)
+                .orElseThrow(GroupNotExist::new);
+        Schedule schedule = scheduleCreateForm.toEntity();
+        schedule.addGroup(group);
+        scheduleRepository.save(schedule);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Event> getGroupSchedule(long groupId, Integer year, Integer month) {
+        if(year == null) year = LocalDateTime.now().getYear();
+        if(month == null) month = LocalDateTime.now().getMonthValue();
+
+        YearMonth yearMonth = YearMonth.of(year, month);
+        LocalDateTime start = yearMonth.atDay(1).atStartOfDay();
+        LocalDateTime end = yearMonth.atEndOfMonth().atTime(LocalTime.MAX);
+        List<Schedule> scheduleList = scheduleRepository.findSchedulesByGroup_IdAndStartTimeBetween(groupId, start, end);
+        return scheduleList.stream().map(Schedule::toEvent)
+                .toList();
+    }
 }
--- src/main/java/com/studyset/web/form/ScheduleCreateForm.java
+++ src/main/java/com/studyset/web/form/ScheduleCreateForm.java
@@ -1,2 +1,31 @@
-package com.studyset.web.form;public class EventCreateForm {
+package com.studyset.web.form;
+
+import com.studyset.domain.Schedule;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ScheduleCreateForm {
+    @NotBlank
+    private String title;
+    private String isImportant = "false";
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String description;
+    private String location;
+
+    public Schedule toEntity(){
+        boolean isImportant = getIsImportant().equals("true") ? true : false;
+        return Schedule.builder()
+                .title(title)
+                .isImportant(isImportant)
+                .description(description)
+                .location(location)
+                .startTime(startDate)
+                .endTime(endDate)
+                .build();
+    }
 }
