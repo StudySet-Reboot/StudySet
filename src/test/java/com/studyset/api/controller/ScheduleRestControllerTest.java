0e002e29960e7577afff8ce12f66bf9f185cff94
kge <kke1231@naver.com>
2024-08-13 17:59:36 +0900

    test: group calendar event service 유닛 테스트 #42

--- src/test/java/com/studyset/api/controller/ScheduleRestControllerTest.java
+++ src/test/java/com/studyset/api/controller/ScheduleRestControllerTest.java
@@ -1,4 +1,80 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.studyset.api.controller;
+
+import com.studyset.domain.User;
+import com.studyset.dto.schedule.Event;
+import com.studyset.service.ScheduleService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest
 class ScheduleRestControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ScheduleService scheduleService;
+
+    @Configuration
+    static class SecurityConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authorizeRequests ->
+                            authorizeRequests.anyRequest().permitAll() // 모든 요청을 허용
+                    ).csrf(AbstractHttpConfigurer::disable);
+            return http.build();
+        }
+    }
+
+    @Test
+    @DisplayName("스케줄 리스트 호출은 JSON으로 응답함")
+    void getSchedule() throws Exception {
+        Long groupId = 1L;
+        Integer year = 2024;
+        Integer month = 8;
+        User user = User.builder()
+                .name("user")
+                .email("test@test.com")
+                .build();
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("user", user);
+        Event event = Event.builder()
+                .title("Event Title")
+                .start(LocalDateTime.of(year, month, 1, 0, 0))
+                .build();
+
+        when(scheduleService.getGroupSchedule(groupId, year, month))
+                .thenReturn(List.of(event));
+
+        mockMvc.perform(get("/api/groups/1/schedules/events")
+                        .param("year", "2024")
+                        .param("month", "8")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Event Title"))
+                .andDo(print()); // 디버깅에 유용하지만, 실제 배포 환경에서는 제거할 수 있음
+    }
 }
--- src/test/java/com/studyset/service/ScheduleServiceTest.java
+++ src/test/java/com/studyset/service/ScheduleServiceTest.java
@@ -1,4 +1,81 @@
+package com.studyset.service;
+
+import com.studyset.domain.Group;
+import com.studyset.domain.Schedule;
+import com.studyset.dto.schedule.Event;
+import com.studyset.repository.GroupRepository;
+import com.studyset.repository.ScheduleRepository;
+import com.studyset.web.form.ScheduleCreateForm;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ScheduleServiceTest {
-  
+
+    @Mock
+    ScheduleRepository scheduleRepository;
+    @Mock
+    GroupRepository groupRepository;
+
+    @InjectMocks
+    ScheduleService scheduleService;
+
+    @Test
+    @DisplayName("스캐줄 추가 폼을 작성하면 폼 객체가 Entity로 변환되어 성공적으로 저장됨")
+    void testAddSchedule() {
+        Group group = new Group();
+        ScheduleCreateForm scheduleCreateForm = new ScheduleCreateForm();
+        scheduleCreateForm.setTitle("Schedule Title");
+        scheduleCreateForm.setDescription("Schedule Description");
+        scheduleCreateForm.setLocation("Schedule Location");
+        when(groupRepository.findGroupById(1l)).thenReturn(Optional.of(group));
+
+        scheduleService.addSchedule(scheduleCreateForm, 1l);
+        ArgumentCaptor<Schedule> scheduleArgumentCaptor = ArgumentCaptor.forClass(Schedule.class);
+        verify(scheduleRepository, times(1)).save(scheduleArgumentCaptor.capture());
+
+
+        Schedule schedule = scheduleArgumentCaptor.getValue();
+        assertNotNull(schedule);
+    }
+
+    @Test
+    @DisplayName("스캐줄 표에 전달할 이벤트들은 Event List로 받음")
+    void testGetScheduleList() {
+        Group group = new Group();
+        int year = 2024;
+        int month = 12;
+        LocalDateTime now = LocalDateTime.of(year, month, 1, 0, 0);
+        LocalDateTime start = LocalDateTime.of(year, month, 1, 0, 0);
+        LocalDateTime end = YearMonth.of(year, month).atEndOfMonth().atTime(LocalTime.MAX);
+        Schedule schedule = Schedule.builder()
+                .title("Schedule Title")
+                .description("Schedule Description")
+                .startTime(now)
+                .isImportant(true)
+                .build();
+
+        when(scheduleRepository.findSchedulesByGroup_IdAndStartTimeBetween(1l, start, end))
+                .thenReturn(List.of(schedule));
+
+        List<Event> eventList =
+                scheduleService.getGroupSchedule(1l, year, month);
+
+        assertEquals(eventList.size(), 1);
+        assertEquals(eventList.get(0).getTitle(), "Schedule Title");
+    }
 }
