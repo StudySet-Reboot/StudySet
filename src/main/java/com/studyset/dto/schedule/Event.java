81401a2cd2760b6d27a7b6eb65c99e597a6535db
kge <kke1231@naver.com>
2024-08-13 17:58:48 +0900

    feat: event list 반환 rest api 추가 #42

--- src/main/java/com/studyset/dto/schedule/Event.java
+++ src/main/java/com/studyset/dto/schedule/Event.java
@@ -1,2 +1,31 @@
-package com.studyset.dto.schedule;public record Event() {
+package com.studyset.dto.schedule;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+
+@Getter
+public class Event {
+    public static final String IMPORTANT_COLOR = "PINK";
+    public static final String NORMAL_COLOR = "GRAY";
+
+    private int id;
+    private String title;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String description;
+    private String color;
+    private String location;
+
+    @Builder
+    public Event(int id, String title, LocalDateTime start, LocalDateTime end, String description, String color, String location) {
+        this.id = id;
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.description = description;
+        this.color = color;
+        this.location = location;
+    }
 }
