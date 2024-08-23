d2c8b233c9bc9d6fdd1f1369b0b7c02cca243d79
gagle1231 <kke1231@naver.com>
2024-08-24 01:12:18 +0900

    feat: 스캐줄 조정표에 해당 주 추가#51

--- src/main/java/com/studyset/controller/ScheduleController.java
+++ src/main/java/com/studyset/controller/ScheduleController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 import java.util.stream.IntStream;
 
 @Controller
@@ -19,16 +22,21 @@ public class ScheduleController {
 
     //스캐줄 캘린더 page
     @GetMapping("/groups/{groupId}/schedules")
-    public String showSchedule(@PathVariable Long groupId, @SessionAttribute GroupDto group,
-                               Model model, @RequestParam(required = false) Integer year, @RequestParam(required = false) Integer month){
-        model.addAttribute("group", group);
+    public String showSchedule(@PathVariable Long groupId, Model model, @RequestParam(required = false) Integer year, @RequestParam(required = false) Integer month){
         return "/thyme/group/schedule/calendar";
     }
 
     //스캐줄 조정 Page
     @GetMapping("/groups/{groupId}/timetables")
-    public String showAdjustPage(@PathVariable Long groupId, @SessionAttribute GroupDto group, @SessionAttribute User user, Model model) {
-        model.addAttribute("group", group);
+    public String showAdjustPage(@PathVariable Long groupId, @SessionAttribute User user, Model model) {
+        LocalDate today = LocalDate.now();
+        int month = today.getMonthValue();
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        int weekOfMonth = today.get(weekFields.weekOfMonth());
+
+        model.addAttribute("month", month);
+        model.addAttribute("weekOfMonth", weekOfMonth);
+
         //user의 가능 시간 찾아서
         int[][] availableTime = scheduleService.getUsersAvailableTime();
         model.addAttribute("times", availableTime);
