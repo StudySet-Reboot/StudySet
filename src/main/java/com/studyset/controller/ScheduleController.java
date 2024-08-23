ba456e143061b21326158741bb5609dadee2858c
gagle1231 <kke1231@naver.com>
2024-08-23 23:13:18 +0900

    feat: 스캐줄 조정표 화면 개발 및 스타일 통일 #51

--- src/main/java/com/studyset/controller/ScheduleController.java
+++ src/main/java/com/studyset/controller/ScheduleController.java
@@ -1,6 +1,6 @@
 package com.studyset.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.studyset.domain.User;
 import com.studyset.dto.group.GroupDto;
 import com.studyset.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +9,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.IntStream;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
-    //main page로
+    //스캐줄 캘린더 page
     @GetMapping("/groups/{groupId}/schedules")
-    public String showSchedule(@PathVariable Long groupId, @SessionAttribute GroupDto group, Model model, @RequestParam(required = false) Integer year, @RequestParam(required = false) Integer month) throws JsonProcessingException {
+    public String showSchedule(@PathVariable Long groupId, @SessionAttribute GroupDto group,
+                               Model model, @RequestParam(required = false) Integer year, @RequestParam(required = false) Integer month){
         model.addAttribute("group", group);
         return "/thyme/group/schedule/calendar";
     }
 
+    //스캐줄 조정 Page
+    @GetMapping("/groups/{groupId}/timetables")
+    public String showAdjustPage(@PathVariable Long groupId, @SessionAttribute GroupDto group, @SessionAttribute User user, Model model) {
+        model.addAttribute("group", group);
+        //user의 가능 시간 찾아서
+        int[][] availableTime = scheduleService.getUsersAvailableTime();
+        model.addAttribute("times", availableTime);
+        return "/thyme/group/schedule/timetable";
+    }
 }
