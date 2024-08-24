16c98c74cb7f656efea4d32e526711db78d970cc
gagle1231 <kke1231@naver.com>
2024-08-24 15:42:37 +0900

    feat: validation 메세지 추가

--- src/main/java/com/studyset/api/request/schedule/ScheduleCreateForm.java
+++ src/main/java/com/studyset/api/request/schedule/ScheduleCreateForm.java
@@ -13,10 +13,10 @@ import java.time.LocalDateTime;
 @Data
 @Slf4j
 public class ScheduleCreateForm {
-    @NotBlank
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
     private Boolean isImportant;
-    @NotNull
+    @NotNull(message = "시작일을 입력해주세요")
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String description;
