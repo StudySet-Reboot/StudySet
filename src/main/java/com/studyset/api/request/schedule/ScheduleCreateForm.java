7e626c85e67dd2579ba413e9e489a66a7b51bc77
gagle1231 <kke1231@naver.com>
2024-08-24 14:50:36 +0900

    refact: request validation 추가 및 변경

--- src/main/java/com/studyset/api/request/schedule/ScheduleCreateForm.java
+++ src/main/java/com/studyset/api/request/schedule/ScheduleCreateForm.java
@@ -2,6 +2,8 @@ package com.studyset.api.request.schedule;
 
 import com.studyset.domain.Schedule;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.bind.DefaultValue;
@@ -14,6 +16,7 @@ public class ScheduleCreateForm {
     @NotBlank
     private String title;
     private Boolean isImportant;
+    @NotNull
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String description;
--- src/main/java/com/studyset/api/request/schedule/ScheduleEditRequest.java
+++ src/main/java/com/studyset/api/request/schedule/ScheduleEditRequest.java
@@ -1,6 +1,8 @@
 package com.studyset.api.request.schedule;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,10 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ScheduleEditRequest {
+    @NotBlank
     private String title;
     private Boolean isImportant;
+    @NotNull
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String description;
