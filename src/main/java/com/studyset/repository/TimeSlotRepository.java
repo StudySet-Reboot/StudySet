08ce647c54180b5c4d1aa7e6cc129ffb3e0fcf10
gagle1231 <kke1231@naver.com>
2024-08-31 11:05:55 +0900

    feat: TimeSlot Data 저장 및 변환 방식 변경 #63
    - 기존: 월, 시작시간, 종료시간로 시간 단위로 저장
        - DB에 너무 많은 데이터가 저장됨, 변환 시간이 오래걸림
    - 변경: 24*7 배열을 이진수 문자열로 저장
    - 화면으로 보낼 시 boolean[][] 배열로 변환

--- src/main/java/com/studyset/repository/TimeSlotRepository.java
+++ src/main/java/com/studyset/repository/TimeSlotRepository.java
@@ -1,2 +1,14 @@
-package com.studyset.repository;public class TimeSlotRepository {
+package com.studyset.repository;
+
+import com.studyset.domain.TimeSlot;
+import com.studyset.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
+    public Optional<TimeSlot> findTimeSlotByUserAndGroupId(User user, Long groupId);
+
+    public void deleteAllByUserAndGroupId(User user, Long groupId);
 }
