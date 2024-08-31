e15ac23182c084cd845018f2752a59f0864af974
gagle1231 <kke1231@naver.com>
2024-08-31 11:02:49 +0900

    feat: TimeSlot 작성 기능 추가 #63

--- src/main/java/com/studyset/service/ScheduleService.java
+++ src/main/java/com/studyset/service/ScheduleService.java
@@ -2,26 +2,38 @@ package com.studyset.service;
 
 import com.studyset.api.exception.GroupNotExist;
 import com.studyset.api.request.schedule.ScheduleEditRequest;
+import com.studyset.api.request.schedule.TimeAdjustRequest;
 import com.studyset.domain.Group;
 import com.studyset.domain.Schedule;
 import com.studyset.api.response.schedule.Event;
+import com.studyset.domain.TimeSlot;
+import com.studyset.domain.User;
 import com.studyset.repository.GroupRepository;
 import com.studyset.repository.ScheduleRepository;
 import com.studyset.api.request.schedule.ScheduleCreateForm;
+import com.studyset.repository.TimeSlotRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.mapping.Map;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.YearMonth;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
+import static com.studyset.api.request.schedule.TimeAdjustRequest.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final GroupRepository groupRepository;
+    private final TimeSlotRepository timeSlotRepository;
 
     @Transactional
     public void addSchedule(ScheduleCreateForm scheduleCreateForm, long groupId) {
@@ -54,8 +66,39 @@ public class ScheduleService {
         return schedule.toEvent();
     }
 
-    public int[][] getUsersAvailableTime() {
-        int availableTime[][] = new int[24][7];
-        return availableTime;
+    @Transactional(readOnly = true)
+    public boolean[][] getUsersAvailableTime(User user, Long groupId) {
+        Group group = groupRepository.findGroupById(groupId)
+                .orElseThrow(GroupNotExist::new);
+        TimeSlot timeSlot = timeSlotRepository.findTimeSlotByUserAndGroupId(user, groupId)
+                .orElse(createNewTimeSlot(user, group));
+        return timeSlot.getTimeSlots();
+    }
+
+    @Transactional
+    public void addTimeSlots(User user, Long groupId, TimeAdjustRequest timeAdjustRequest) {
+        Group group = groupRepository.findGroupById(groupId)
+                .orElseThrow(GroupNotExist::new);
+        TimeSlot timeSlot = timeSlotRepository.findTimeSlotByUserAndGroupId(user, groupId)
+                .orElse(createNewTimeSlot(user, group));
+        boolean[][] timeslotList = new boolean[24][7];
+
+        for(TimeSlotData data: timeAdjustRequest.getList()){
+            int day = data.getDay();
+            int time = data.getTime();
+            timeslotList[time][day] = true;
+        }
+        timeSlot.setTimeSlots(timeslotList);
+        timeSlotRepository.save(timeSlot);
+    }
+
+    public TimeSlot createNewTimeSlot(User user, Group group){
+        TimeSlot timeSlot = TimeSlot
+                .builder()
+                .user(user)
+                .group(group)
+                .build();
+        timeSlot.setTimeSlots(new boolean[24][7]);
+        return timeSlot;
     }
 }
