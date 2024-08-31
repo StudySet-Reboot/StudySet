3f66909dd36af8985c5bf021216834261f72a778
gagle1231 <kke1231@naver.com>
2024-08-31 11:04:53 +0900

    feat: 스캐줄 조정표 Requset DTO 추가 #63

--- src/main/java/com/studyset/api/request/schedule/TimeAdjustRequest.java
+++ src/main/java/com/studyset/api/request/schedule/TimeAdjustRequest.java
@@ -1,2 +1,22 @@
-package com.studyset.api.request.schedule;public class TimeAdjustRequest {
+package com.studyset.api.request.schedule;
+
+import lombok.*;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TimeAdjustRequest {
+
+    private List<TimeSlotData> list;
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class TimeSlotData{
+        private int day;
+        private int time;
+    }
+
 }
