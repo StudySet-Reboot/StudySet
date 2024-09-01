698075d6ae5f6ad89532549d8abafbf93c1a5e70
gagle1231 <kke1231@naver.com>
2024-09-01 12:47:56 +0900

    feat: Schedule 종료 시간 검증 로직, Exception 추가 #52

--- src/main/java/com/studyset/api/exception/InvalidEndDateException.java
+++ src/main/java/com/studyset/api/exception/InvalidEndDateException.java
@@ -1,2 +1,15 @@
-package com.studyset.api.exception;public class InvalidEndDateException {
+package com.studyset.api.exception;
+
+public class InvalidEndDateException extends BaseException{
+
+    private static final String message = "시작시간은 종료시간보다 앞서야 합니다.";
+
+    public InvalidEndDateException() {
+        super(message);
+    }
+
+    @Override
+    public int statusCode() {
+        return 400;
+    }
 }
