630018b4bb91b3f2ceb9aec383523a80962079ed
hyooojing <81282601+hyooojing@users.noreply.github.com>
2024-09-10 15:10:17 +0900

    feat: 댓글 삭제 #49
    -댓글 삭제 (수정 불가)

--- src/main/java/com/studyset/controller/CommentController.java
+++ src/main/java/com/studyset/controller/CommentController.java
@@ -1,27 +1,20 @@
 package com.studyset.controller;
 
-import com.studyset.api.exception.UserNotExist;
 import com.studyset.domain.User;
-import com.studyset.dto.memo.MemoDto;
 import com.studyset.dto.task.CommentDto;
 import com.studyset.repository.UserRepository;
 import com.studyset.service.CommentService;
 import com.studyset.web.form.CommentForm;
-import com.studyset.web.form.MemoCreateForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Controller()
+@RestController
 @RequestMapping("/groups")
 @RequiredArgsConstructor
 @Slf4j
@@ -31,7 +24,6 @@ public class CommentController {
 
     // 댓글 작성
     @PostMapping("/userTask/addComment")
-    @ResponseBody
     public ResponseEntity<Map<String, Object>> addComment(@RequestBody CommentForm comment) {
         CommentDto newComment = commentService.addComment(comment);
         // 새로운 유저의 이름을 포함해 응답
@@ -44,5 +36,10 @@ public class CommentController {
     }
 
     // 댓글 삭제
-
+    @DeleteMapping("/userTask/{commentId}/deleteComment")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+        log.info("Deleting comment: {}", commentId);
+        commentService.deleteComment(commentId);
+        return ResponseEntity.noContent().build();
+    }
 }
