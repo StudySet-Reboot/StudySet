b02debb2f6d04d9029682feffd4de55e16bebb8e
gagle1231 <kke1231@naver.com>
2024-09-13 00:39:16 +0900

    feat: group 회비 내역 화면 개발 #74

--- src/main/java/com/studyset/controller/DuesController.java
+++ src/main/java/com/studyset/controller/DuesController.java
@@ -1,4 +1,32 @@
 package com.studyset.controller;
 
+import com.studyset.domain.Group;
+import com.studyset.domain.User;
+import com.studyset.dto.group.GroupDto;
+import com.studyset.dto.user.UserDto;
+import com.studyset.service.DuesService;
+import com.studyset.service.GroupService;
+import com.studyset.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class DuesController {
+    private final DuesService duesService;
+    private final JoinService joinService;
+
+    @GetMapping("/groups/{groupId}/dues")
+    public String getDeusPage(@PathVariable Long groupId, Model model) {
+        List<UserDto> userList = joinService.getUserByGroupId(groupId);
+        model.addAttribute("userList", userList);
+        return "thyme/dues/dues";
+    }
 }
