6ae3affddd2466d40b5218044f98d4d5c452123e
gagle1231 <kke1231@naver.com>
2024-09-16 03:06:18 +0900

    feat: 회비 사용 내역 화면 개발 #78

--- src/main/resources/static/js/dues/dues.js
+++ src/main/resources/static/js/dues/dues.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var btn = document.getElementById("create-modal-btn");
     var createModal = document.getElementById("create-modal");
     var closeButtons = document.querySelectorAll(".close-btn");
+    var showPaymentBtn = document.getElementById("show-payment-btn");
+    var showDuesBtn = document.getElementById("show-dues-btn");
+
+    var groupId = document.getElementById("groupId").value;
 
     btn.onclick = function() {
         createModal.style.display = "block";
@@ -12,6 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
             btn.parentElement.parentElement.style.display = "none";
         }
     });
+
+    showPaymentBtn.onclick = function () {
+        location.href=`/groups/${groupId}/payment`;
+    }
+
+    showDuesBtn.onclick = function () {
+        location.href=`/groups/${groupId}/dues`;
+    }
 
     window.onclick = function(event) {
         if (event.target == createModal) {
