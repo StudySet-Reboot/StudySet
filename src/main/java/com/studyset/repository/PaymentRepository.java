f95116a757ee1165e9cc440872274aecea34dde6
gagle1231 <kke1231@naver.com>
2024-09-16 04:32:16 +0900

    feat: 회비 사용 내역 리스트 가져오기, 회비 사용 내역 추가 기능 #78

--- src/main/java/com/studyset/repository/PaymentRepository.java
+++ src/main/java/com/studyset/repository/PaymentRepository.java
@@ -1,4 +1,12 @@
 package com.studyset.repository;
 
-public interface PaymentRepository {
+import com.studyset.domain.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Page<Payment> findByGroupId(Long groupId, Pageable pageable);
+
 }
--- src/main/resources/static/js/dues/dues.js
+++ src/main/resources/static/js/dues/dues.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     showPaymentBtn.onclick = function () {
-        location.href=`/groups/${groupId}/payment`;
+        location.href=`/groups/${groupId}/payments`;
     }
 
     showDuesBtn.onclick = function () {
@@ -31,3 +31,23 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+function deleteItem(itemId, type) {
+    var groupId = document.getElementById("groupId").value;
+    if(type === "payment"){
+        if (confirm("이 항목을 삭제하시겠습니까?")) {
+
+            fetch(`/groups/${groupId}/payments/${itemId}`, {
+                method: 'DELETE'
+            }).then(response => {
+                if (response.ok) {
+                    alert('삭제 성공!');
+                } else {
+                    alert('삭제에 실패했습니다.');
+                }
+            }).catch(error => {
+                console.error('삭제 요청 실패:', error);
+            });
+        }
+    }
+}
