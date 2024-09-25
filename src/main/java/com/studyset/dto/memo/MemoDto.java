4d87548da11a6557957757f043be7addda395656
kge <kke1231@naver.com>
2024-09-25 17:38:27 +0900

    feat: memo username filed 추가 #80

--- src/main/java/com/studyset/dto/memo/MemoDto.java
+++ src/main/java/com/studyset/dto/memo/MemoDto.java
@@ -8,12 +8,14 @@ public class MemoDto {
 
     private Long groupId;
     private Long userId;
+    private String userName;
     private String contents;
 
     @Builder
-    public MemoDto(Long groupId, Long userId, String contents) {
+    public MemoDto(Long groupId, Long userId, String userName, String contents) {
         this.groupId = groupId;
         this.userId = userId;
+        this.userName = userName;
         this.contents = contents;
     }
 }
