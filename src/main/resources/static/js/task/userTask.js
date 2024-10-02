d278abc478d298ee3c1ebfb63782fb5180ed33f4
gagle1231 <kke1231@naver.com>
2024-10-02 22:06:02 +0900

    feat: modal css 모듈 분리 #76

--- src/main/resources/static/js/task/userTask.js
+++ src/main/resources/static/js/task/userTask.js
@@ -1,3 +1,5 @@
+import {displayErrorToast, showConfirm} from "../common/toast.js";
+
 document.addEventListener('DOMContentLoaded', function() {
     const deleteButton = document.getElementById('deleteTaskButton');
 
@@ -5,34 +7,46 @@ document.addEventListener('DOMContentLoaded', function() {
         deleteButton.addEventListener('click', function(event) {
             event.preventDefault();
 
-            // 사용자에게 삭제 확인 메시지 표시
-            const confirmation = confirm("정말로 이 과제를 삭제하시겠습니까?");
+            showConfirm(function(result) {
+                if (result) {
+                    fetch(`/groups/submission/${taskId}/${userId}/deleteSubmission`, {
+                        method: 'DELETE'
+                    }).then(response => {
+                        displayErrorToast("과제를 삭제하였습니다.")
+                        setTimeout(()=> window.location.href = `/groups/${groupId}/${taskId}/taskDetail`,
+                            1000);
+                    }).catch(error => displayErrorToast(error));
+                }
+            });
 
-            if (confirmation) {
-                // 사용자가 확인을 클릭한 경우에만 삭제 요청을 보냄
-                fetch(`/groups/submission/${taskId}/${userId}/deleteSubmission`, {
-                    method: 'DELETE'
-                })
-                    .then(response => {
-                        if (response.ok) {
-                            // 삭제 성공 시 리다이렉트
-                            alert("과제를 삭제하였습니다.");
-                            window.location.href = `/groups/${groupId}/${taskId}/taskDetail`;
-                        } else {
-                            // 오류 처리
-                            return response.text().then(text => {
-                                throw new Error(text);
-                            });
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                        alert('과제 삭제에 실패했습니다: ' + error.message);
-                    });
-            } else {
-                // 사용자가 취소를 클릭한 경우 아무 작업도 하지 않음
-                alert("과제 삭제를 취소하였습니다.");
-            }
+            // 사용자에게 삭제 확인 메시지 표시
+            // const confirmation = confirm("정말로 이 과제를 삭제하시겠습니까?");
+            //
+            // if (confirmation) {
+            //     // 사용자가 확인을 클릭한 경우에만 삭제 요청을 보냄
+            //     fetch(`/groups/submission/${taskId}/${userId}/deleteSubmission`, {
+            //         method: 'DELETE'
+            //     })
+            //         .then(response => {
+            //             if (response.ok) {
+            //                 // 삭제 성공 시 리다이렉트
+            //                 alert("과제를 삭제하였습니다.");
+            //                 window.location.href = `/groups/${groupId}/${taskId}/taskDetail`;
+            //             } else {
+            //                 // 오류 처리
+            //                 return response.text().then(text => {
+            //                     throw new Error(text);
+            //                 });
+            //             }
+            //         })
+            //         .catch(error => {
+            //             console.error('Error:', error);
+            //             alert('과제 삭제에 실패했습니다: ' + error.message);
+            //         });
+            // } else {
+            //     // 사용자가 취소를 클릭한 경우 아무 작업도 하지 않음
+            //     alert("과제 삭제를 취소하였습니다.");
+            // }
         });
     }
 
@@ -45,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const isChecked = checkbox.checked;
 
         if (commContent === '') {
-            alert('댓글을 작성해 주세요.');
+            displayErrorToast('댓글을 작성해 주세요.');
             return;
         }
 
@@ -153,7 +167,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         } catch (error) {
             console.error('Error:', error);
-            alert('댓글 전송 중 오류가 발생했습니다.');
+            displayErrorToast('댓글 전송 중 오류가 발생했습니다.');
         }
     }
 
@@ -187,12 +201,12 @@ document.addEventListener('DOMContentLoaded', function() {
                             // 댓글이 성공적으로 삭제되면 뷰에서 해당 댓글 요소 제거
                             commentBox.remove();
                         } else {
-                            alert('댓글 삭제에 실패했습니다.');
+                            displayErrorToast('댓글 삭제에 실패했습니다.');
                         }
                     })
                     .catch(error => {
                         console.error('Error deleting comment:', error);
-                        alert('댓글 삭제 중 오류가 발생했습니다.');
+                        displayErrorToast('댓글 삭제 중 오류가 발생했습니다.');
                     });
             }
         }
