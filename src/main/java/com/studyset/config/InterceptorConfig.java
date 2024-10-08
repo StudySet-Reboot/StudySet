cf256665f94fa6631825d66a91c8b7f979f92848
gagle1231 <kke1231@naver.com>
2024-10-09 00:28:31 +0900

    refactor: Interceptor code refactoring #101
    - InterceptorConfig의 GroupAccessInterceptor DI 방식 수정
        - 기존: 필드 주입
        - 변경: 생성자 주입
    - GroupAccessInterceptor 함수 분리, 예외 처리 개선

--- src/main/java/com/studyset/config/InterceptorConfig.java
+++ src/main/java/com/studyset/config/InterceptorConfig.java
@@ -1,16 +1,17 @@
 package com.studyset.config;
 
 import com.studyset.interceptor.GroupAccessInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private GroupAccessInterceptor groupInterceptor;
+    private final GroupAccessInterceptor groupInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
