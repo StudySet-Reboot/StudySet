106d1b723d773c63d3669a07f4496c2f7725756e
gagle1231 <kke1231@naver.com>
2024-10-09 00:40:39 +0900

    feat: auth exception #81

--- src/main/java/com/studyset/exception/ForbiddenAccess.java
+++ src/main/java/com/studyset/exception/ForbiddenAccess.java
@@ -1,2 +1,20 @@
-package com.studyset.exception;public class ForbiddenAccess {
+package com.studyset.exception;
+
+import com.studyset.exception.response.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public class ForbiddenAccess extends BaseException{
+
+    public ForbiddenAccess(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public ForbiddenAccess(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+
+    @Override
+    public HttpStatus statusCode() {
+        return HttpStatus.FORBIDDEN;
+    }
 }
