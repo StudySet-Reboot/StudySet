9eba807ffb3f8ac92b8294baefedf5f59e9aaa6c
gagle1231 <kke1231@naver.com>
2024-10-13 23:53:15 +0900

    docs: Group Service JavaDocs 추가 #101

--- src/main/java/com/studyset/service/JoinService.java
+++ src/main/java/com/studyset/service/JoinService.java
@@ -2,7 +2,6 @@ package com.studyset.service;
 
 import com.studyset.domain.User;
 import com.studyset.dto.user.UserDto;
-import com.studyset.repository.GroupRepository;
 import com.studyset.repository.UserJoinGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +12,15 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class JoinService {
-    private final GroupRepository groupRepository;
+
     private final UserJoinGroupRepository joinGroupRepository;
 
-    // 그룹 내 그룹원 조회
+    /**
+     * 그룹 ID를 통해 그룹에 속한 사용자 목록을 조회합니다.
+     *
+     * @param groupId 그룹의 ID
+     * @return List<UserDto> 그룹에 속한 사용자 정보
+     */
     public List<UserDto> getUserByGroupId(Long groupId) {
         List<User> users = joinGroupRepository.findUsersByGroupId(groupId);
         return users.stream().map(UserDto::fromUser).collect(Collectors.toList());
