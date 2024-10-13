aa45bc32b8c38119c6958534e0505beb028f62e0
gagle1231 <kke1231@naver.com>
2024-10-14 00:43:22 +0900

    docs: Add comments to PaymentService and DuesService #101

--- src/main/java/com/studyset/service/PaymentService.java
+++ src/main/java/com/studyset/service/PaymentService.java
@@ -21,12 +21,25 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final GroupRepository groupRepository;
 
+    /**
+     *  그룹의 회비 사용 내역 목록을 페이지로 반환합니다.
+     *
+     * @param groupId 그룹의 ID
+     * @param pageable 페이징 정보
+     * @return Page<PaymentDto> 회비 사용 내역 목록 DTO
+     */
     @Transactional(readOnly = true)
     public Page<PaymentDto> getPayments(Long groupId, Pageable pageable) {
         return paymentRepository.findByGroupId(groupId, pageable)
                 .map(Payment::toPaymentDto);
     }
 
+    /**
+     * 회비 사용 내역을 추가합니다.
+     *
+     * @param groupId     회비 사용 내역이 추가될 그룹의 ID
+     * @param paymentForm 회비 사용 내역을 담은 form 객체
+     */
     public void addPayment(Long groupId, PaymentForm paymentForm) {
         Group group = groupRepository.findGroupById(groupId)
                 .orElseThrow(GroupNotExist::new);
