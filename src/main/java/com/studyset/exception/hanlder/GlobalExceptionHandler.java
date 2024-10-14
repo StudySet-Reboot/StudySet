74cc5ec34e51540c39b24229f5329ec72507cba4
gagle1231 <kke1231@naver.com>
2024-10-14 20:58:51 +0900

    style: remove unused import #108

--- src/main/java/com/studyset/exception/hanlder/GlobalExceptionHandler.java
+++ src/main/java/com/studyset/exception/hanlder/GlobalExceptionHandler.java
@@ -1,22 +1,13 @@
 package com.studyset.exception.hanlder;
 
-import ch.qos.logback.core.model.processor.ModelHandlerException;
 import com.studyset.exception.GroupCodeError;
 import com.studyset.exception.UserNotExist;
 import com.studyset.exception.response.ErrorResponse;
-import jakarta.servlet.http.HttpServletRequest;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.HashMap;
 
 @ControllerAdvice
 @Slf4j
