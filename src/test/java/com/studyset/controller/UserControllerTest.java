180bafb6657ae176a3fd05d22ae21f5f75edc25b
gagle1231 <kke1231@naver.com>
2024-10-27 13:48:22 +0900

    test: controller test 에러 수정#125
    - 프로젝트 설정과 수정에 따라 기존 테스트 수정 필요
    - ScheduleRestControllerTest 오타 수정
    - GroupControllerTest 그룹 코드 추가
    - UserControllerTest 빈 test 추가
    - TaskControllerTest exception Handling 수정

--- src/test/java/com/studyset/controller/UserControllerTest.java
+++ src/test/java/com/studyset/controller/UserControllerTest.java
@@ -1,42 +1,85 @@
 package com.studyset.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.studyset.service.GroupService;
+import com.studyset.domain.User;
+import com.studyset.dto.group.GroupDto;
 import com.studyset.service.JoinService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
+
+    @Mock
     private JoinService joinService;
-    @Autowired
-    private ObjectMapper objectMapper;
+
+    private User user;
+    private MockHttpServletRequest request;
+
     @BeforeEach
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new UserController(joinService)).build();
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        user = new User();
+        user.setId(1L); // 필요한 사용자 정보 설정
+
+        request = new MockHttpServletRequest();
+        request.getSession().setAttribute("user", user);
     }
 
-    //TODO: Test Code 작성 필요
     @Test
-    public void test() throws Exception {
-        //given
-        //when
-        /*
-        mockMvc.perform(post("/groups/create")
-                        .param("groupName", "Test Group")
-                        .param("description", "This is a test group"))
+    @DisplayName("유저 메인 페이지 조회 성공")
+    void userMain_Success() throws Exception {
+        // given
+        List<GroupDto> groupDtoList = new ArrayList<>();
+        groupDtoList.add(new GroupDto());
 
-                //then
+        Page<GroupDto> groups = new PageImpl<>(groupDtoList);
+        when(joinService.getUserGroupList(ArgumentMatchers.any(User.class), ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(groups);
+
+        // when & then
+        mockMvc.perform(get("/users/main")
+                        .sessionAttr("user", user))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("groups"))
+                .andExpect(model().attributeExists("currentPage"))
+                .andExpect(model().attributeExists("totalPages"))
+                .andExpect(model().attributeExists("totalItems"));
+    }
+
+    @Test
+    @DisplayName("유저 세션이 없으면 로그인 페이지로 리다이렉트")
+    void userMain_NoUserSession_RedirectToLogin() throws Exception {
+        // given
+        request.getSession().invalidate();
+
+        // when & then
+        mockMvc.perform(get("/users/main")
+                        .requestAttr("request", request))
                 .andExpect(status().is3xxRedirection())
-                .andDo(print());
-        verify(groupService, times(1)).createGroup(any(GroupCreateForm.class)); */
+                .andExpect(view().name("redirect:/login"));
     }
 }
